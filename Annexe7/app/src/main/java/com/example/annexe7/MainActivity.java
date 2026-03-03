56a32be982c892e9d10fbde939fa2bdc2e9354ca
felix <larosefelixb@gmail.com>
2026-03-03 10:33:15 -0500

    annexe7 fin

--- Annexe7/app/src/main/java/com/example/annexe7/MainActivity.java
+++ Annexe7/app/src/main/java/com/example/annexe7/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.annexe7;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Point;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 
 import androidx.activity.EdgeToEdge;
@@ -15,8 +19,11 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    Ecouteur ec;
     SurfaceDessin surf;
     ConstraintLayout main;
+
+    Point depart, arrivee;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,24 +37,60 @@ public class MainActivity extends AppCompatActivity {
 
         main = findViewById(R.id.main);
 
+        // ecouteur
+        ec = new Ecouteur();
+
+
         // 1
         surf = new SurfaceDessin(this);
         // 2
         surf.setLayoutParams(new ConstraintLayout.LayoutParams(-1 , -1));
         // 3
         main.addView(surf);
+        surf.setOnTouchListener(ec);
+    }
+
+    private class Ecouteur implements View.OnTouchListener
+    {
+        @Override
+        public boolean onTouch(View v, MotionEvent motionEvent) {
+
+            if (motionEvent.getAction() == MotionEvent.ACTION_DOWN){
+                depart = new Point((int)motionEvent.getX(), (int) motionEvent.getY());
+                surf.invalidate();  // redessiner --> efface et appel la méthode onDraw
+            } else if (motionEvent.getAction() == MotionEvent.ACTION_MOVE) {
+                arrivee = new Point((int) motionEvent.getX(), (int) motionEvent.getY());
+                surf.invalidate();  // redessiner --> efface et appel la méthode onDraw
+            } else if (motionEvent.getAction() == MotionEvent.ACTION_UP) {
+                depart = null;
+                arrivee = null;
+            }
+
+            // important!
+            return true;
+        }
     }
 
     private class SurfaceDessin extends View {
-
+        private Paint p = new Paint(Paint.ANTI_ALIAS_FLAG);
         public SurfaceDessin (Context context){
             super(context);
             this.setBackgroundResource(R.drawable.carte);
+            p.setColor(Color.RED);
+            p.setStrokeWidth(15);
         }
 
         @Override
         protected void onDraw(@NonNull Canvas canvas){
             super.onDraw(canvas);
+            if (depart != null){
+                canvas.drawRect(depart.x-30, depart.y+30, depart.x+30, depart.y-30, p);
+            }
+
+            if (arrivee != null){
+                canvas.drawRect(arrivee.x-30, arrivee.y+30, arrivee.x+30, arrivee.y-30, p);
+                canvas.drawLine(depart.x, depart.y, arrivee.x, arrivee.y, p);
+            }
         }
     }
 }
